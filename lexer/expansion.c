1d3feb7b4de62e48a7f18ebd4efcd8394201e486
Raimund Koop <rkoop@student.42heilbronn.de>
2022-11-27 15:38:54 +0100

    start more builtins

--- lexer/expansion.c
+++ lexer/expansion.c
@@ -1,25 +1,31 @@
 # include "../includes/shell.h"
 #include <string.h>
-//expansion of variables within DQUOTES
-// $? -> exit_status of last command
-// $$ -> just $
+#include <stdio.h>
+
+//current problem: long expansion after short one in same cmd
+
+
 char	*get_var(t_data *data, char *var)
 {
 	t_env 	*current_env;
 	char	*value;
+	size_t len_var;
 
+	len_var = ft_strlen(var);
 	current_env = data->env_tesh;
 	value = NULL;
+	//// $? -> exit_status of last command
+	// $$ -> just $
 	while (current_env != NULL)
 	{
 		//check if given var is in current list elem->var. ! additional check for case, char *var is empty
 		if (ft_strnstr(current_env->var, var + 1, ft_strlen(current_env->var)) != NULL)
 		{
-			value = malloc(sizeof(char) * ((ft_strlen(current_env->var) - ft_strlen(var))));
+			value = malloc(sizeof(char) * ((ft_strlen(current_env->var) - len_var)));
 			if (value == NULL)
 				exit(ENOMEM);
 			//copy var string starting at strt to value
-			value = ft_substr(current_env->var, ft_strlen(var), ft_strlen(current_env->var) - ft_strlen(var));
+			value = ft_substr(current_env->var, len_var, ft_strlen(current_env->var) - len_var);
 			break ;
 		}
 		current_env = current_env->next;
@@ -28,42 +34,38 @@ char	*get_var(t_data *data, char *var)
 }
 
 
-// modify remove function to remove from start_index
-char *str_remove(char *str, const char *sub) {
-    char *p, *q, *r;
-    if (*sub && (q = r = ft_strnstr(str, sub, ft_strlen(str))) != NULL) {
-        size_t len = (size_t) ft_strlen(sub);
-        while ((r = ft_strnstr(p = r + len, sub, ft_strlen(str))) != NULL) {
-            while (p < r)
-                *q++ = *p++;
-        }
-        while ((*q++ = *p++) != '\0')
-            continue;
-    }
-    return str;
+void  *ft_str_remove(char *str, const char *sub)
+{
+    //ptr to start of part to be removed
+    char *start = ft_strnstr(str, sub, ft_strlen(str));
+    //ptr to end
+    char *end = start + ft_strlen(sub);
+    //concecnate: copy end part to start part
+    ft_strncpy(start, end, ft_strlen(str));
 }
-
-// char	ft_str_remove(char *str, char *substr, int start_index)
-// {
-
-// }
 
 void	insert_value(t_token *token, char *var, char *value, int start_index)
 {
+	//lens as variables to avoid multiple strlen calls
+	size_t len_token_name;
+	size_t len_value;
+
+	len_token_name = ft_strlen(token->name);
+	//+1 cause of $ in beginng
+	len_value = ft_strlen(value) + 1;
 	//remove $var from token->name
-	str_remove(token->name, var);
-	
+	ft_str_remove(token->name, var);
 	//second: insert value
 	//allocate new string for concenated one
-	char *name_expanded = malloc(sizeof(char) * (ft_strlen(token->name) + ft_strlen(value) + 1));
+	char *name_expanded = malloc(sizeof(char) * (len_token_name + len_value));
 	if (name_expanded == NULL)
 		exit(ENOMEM);
 	//copy first strA until start to new strC, nullterminate
 	ft_strlcpy(name_expanded, token->name, start_index + 1);
 	//strlcat to copy strB
-	ft_strlcat(name_expanded, value, (size_t)(ft_strlen(token->name) + ft_strlen(value) + 1));
+	ft_strlcat(name_expanded, value, (len_token_name + len_value));
 	//strlcat to copy rest of strB (strcC, strA + x)
-	ft_strlcat(name_expanded, token->name + start_index, (size_t)(ft_strlen(token->name) + ft_strlen(value) + 1));
+	ft_strlcat(name_expanded, token->name + start_index, (len_token_name + len_value));
 	//free & replace token name with extended str
 	free(token->name);
 	token->name = name_expanded;
@@ -86,21 +88,20 @@ void	expand_tokens(t_data *data, t_token *token)
 	var = NULL;
 	value = NULL;
 	start_index = 0;
+	var = malloc(sizeof(char) * 5);
 
 	while (token->name[i] != '\0')
 	{
 		if (token->name[i] == '$')
 		{
-			//dprintf(2, "\n $ found");
 			start = i;
 			while (token->name[i] != ' ' && token->name[i] != '\0')
 				i++;
 			end = i;
-			var = malloc(sizeof(char) * (end - start));
+			var = (char *)realloc(var, sizeof(char) * (end - start));
 			if (var == NULL)
 				exit(ENOMEM);
 			start_index = start;
-			dprintf(2, "\n var before: %s", var + 1);
 			while(start < end)
 			{
 				var[j] = token->name[start];
@@ -108,20 +109,18 @@ void	expand_tokens(t_data *data, t_token *token)
 				start++;
 			}
 			j = 0;
+			//look up variable in ENV
 			value = get_var(data, var);
-			dprintf(2, "\n var from name: %s", var + 1);
-			dprintf(2, "\n val from get_var: %s", value);
 			//modify token: insert value into token->name
 			insert_value(token, var, value, start_index);
 			dprintf(2, "\n token name: %s", token->name);
 			dprintf(2, "\n -----------\n");
-			ft_memset(var, 0, ft_strlen(var));
-			free(var);
-			var = " ";
 			free(value);
 		}
 		i++;
 	}
+	if (var != NULL)
+		free(var);
 }
 
 void	expansion(t_data *data)
